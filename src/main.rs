71d34323668123ce236f67ae36a547187469e864
Tom Streller <tom@streller.me>
2021-12-05 17:19:26 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+};
+
+fn main() {
+    let mut app = App::build();
+    app.insert_resource(Msaa { samples: 4 })
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(WindowDescriptor {
+            width: 1280.0,
+            height: 720.0,
+            title: "Roguelike!".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins);
+
+    #[cfg(debug_assertions)]
+    {
+        app.add_plugin(FrameTimeDiagnosticsPlugin::default())
+            .add_plugin(LogDiagnosticsPlugin::default());
+    }
+
+    app.run();
+}
